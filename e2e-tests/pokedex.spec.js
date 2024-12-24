6184b5ae7af70e810cacd8a26c3c7c8bd784584e
David Pérez <deividpa022@gmail.com>
2024-12-24 00:05:38 -0500

    feat: add initial Pokedex end-to-end test for front page visibility

---
+++ e2e-tests/pokedex.spec.js
@@ -0,0 +1,10 @@
+const { test, expect } = require('@playwright/test')
+
+test.describe('Pokedex', () => {
+  test('front page can be opened', async ({ page }) => {
+    await page.goto('http://localhost:8080')
+
+    await expect(page.getByText('ivysaur')).toBeVisible()
+    await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
+  })
+})
